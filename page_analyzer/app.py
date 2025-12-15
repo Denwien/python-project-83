31374426e403ad8b9bf829bef4af8c421cc0dbca
Denwien <denisgrigoriev1973@gmail.com>
2025-12-15 17:07:32 +0100

    Добавлен шаблон главной страницы с Bootstrap

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
 load_dotenv()
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
-
 @app.route("/")
 def index():
-    return "Hello, Hexlet!"
+    return render_template("index.html")
