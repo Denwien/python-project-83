0d73d01def05a46ddd07ea153ea076ef602bd6a9
Denwien <denisgrigoriev1973@gmail.com>
2025-12-15 19:05:53 +0100

    Add database, urls, validation

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-# скачиваем uv и устанавливаем зависимости
+
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
+
+make install && psql -a -d $DATABASE_URL -f database.sql
