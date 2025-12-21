3ea041957dbdcf03c5ab748b4fe5d58bc4379e12
Denwien <denisgrigoriev1973@gmail.com>
2025-12-21 09:17:46 +0100

    Обновил build.sh

--- build.sh
+++ build.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 set -e
 
+curl -LsSf https://astral.sh/uv/install.sh | sh
+. $HOME/.local/bin/env
+
 if [ -z "$DATABASE_URL" ]; then
   echo "Ошибка: DATABASE_URL не установлена"
   exit 1
 fi
 
-uv sync
-python -m compileall .
-
-python <<'EOF'
-import os
-import psycopg
-
-DATABASE_URL = os.environ.get("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL не установлена")
-
-init_file = "init.sql"
-if os.path.exists(init_file):
-    with psycopg.connect(DATABASE_URL) as conn:
-        with conn.cursor() as cur:
-            cur.execute(open(init_file).read())
-        conn.commit()
-EOF
+make install
+psql -a -d "$DATABASE_URL" -f database.sql
