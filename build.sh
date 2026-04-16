7249408fbd9c62a6c6c1147220b5e0e2cdb8d20b
Denwien <Dsch0rk131973@users.noreply.github.com>
2026-04-16 22:36:41 +0300

    Fix hexlet-check by restoring uv project config

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-pip install poetry
-poetry install && psql -a -d "$DATABASE_URL" -f database.sql
+# Установка uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# Вместо source используем точку, которая работает во всех POSIX-совместимых оболочках
+. "$HOME/.local/bin/env"
+
+# Установка и настройка базы данных
+make install && psql -a -d "$DATABASE_URL" -f database.sql
